f0008d6b65233aa65625cced31b557df1380efad
bheili <brian@heili.com>
2018-01-18 14:20:02 -0600

    Update

---
+++ rds_config.py
@@ -0,0 +1,5 @@
+#config file containing credentials for rds mysql instance
+db_username = "dbadmin"
+db_password = "VMware1!"
+db_name = "CorpDB" 
+db_host  = "corpdb.cdecruvpkapc.us-west-1.rds.amazonaws.com"
